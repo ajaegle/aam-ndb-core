08cb07350766eb0d34947aa05e08ac28b26b693e
TheSlimvReal <TheSlimvReal@users.noreply.github.com>
2020-11-02 21:39:06 +0000

    deploy: eaec8968799d9cd644f69ddfc7a9593c89b2f2fc

--- deployments/dynamic-basic-info/main.js
+++ deployments/dynamic-basic-info/main.js
@@ -5945,6 +5945,26 @@ var __generator = (undefined && undefined.__generator) || function (thisArg, bod
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
+var __read = (undefined && undefined.__read) || function (o, n) {
+    var m = typeof Symbol === "function" && o[Symbol.iterator];
+    if (!m) return o;
+    var i = m.call(o), r, ar = [], e;
+    try {
+        while ((n === void 0 || n-- > 0) && !(r = i.next()).done) ar.push(r.value);
+    }
+    catch (error) { e = { error: error }; }
+    finally {
+        try {
+            if (r && !r.done && (m = i["return"])) m.call(i);
+        }
+        finally { if (e) throw e.error; }
+    }
+    return ar;
+};
+var __spread = (undefined && undefined.__spread) || function () {
+    for (var ar = [], i = 0; i < arguments.length; i++) ar = ar.concat(__read(arguments[i]));
+    return ar;
+};
 
 
 
@@ -6244,9 +6264,9 @@ var FormComponent = /** @class */ (function () {
     };
     FormComponent.prototype.buildFormConfig = function () {
         var _this = this;
-        // TODO save after first computation
         var formConfig = {};
-        this.config.cols.flat().forEach(function (c) {
+        // Flattening the cols array, array.flat() is not yet available in current browsers
+        [].concat.apply([], __spread(this.config.cols)).forEach(function (c) {
             formConfig[c.id] = [{ value: _this.child[c.id], disabled: !_this.editing }];
             if (c.required) {
                 formConfig[c.id].push(_angular_forms__WEBPACK_IMPORTED_MODULE_2__["Validators"].required);
